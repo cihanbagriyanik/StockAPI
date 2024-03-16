e276413646e81873f96106097abfdcf1840dec31
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2024-03-16 02:02:19 +0100

    origin up

--- index.js
+++ index.js
@@ -46,9 +46,9 @@ app.use(
   cors({
     origin: [
       "https://stock-management-app-cihan.vercel.app",
-      "https://stock-management-app-react-redux-formik-6logrn17n.vercel.app/",
-      "http://localhost:5173/",
-      "http://localhost:3000/",
+      "https://stock-management-app-react-redux-formik-6logrn17n.vercel.app",
+      "http://localhost:5173",
+      "http://localhost:3000",
     ],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   })
